f08c0407ea9aeb46599e95a9253ebbcd0277a7d1
tanran <51186467+tanranv5@users.noreply.github.com>
2026-01-14 17:25:22 +0800

    ci: do not fail build on GCC warnings

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -51,15 +51,8 @@ def interpret_warning(line):
     line = line.rstrip('\n')
     m = warning_re.match(line)
     if m and m.group(2) not in allowed_warnings:
-        print("error, forbidden warning:", m.group(2), file=sys.stderr)
-
-        # If there is a warning, remove any object if it exists.
-        if ofile:
-            try:
-                os.remove(ofile)
-            except OSError:
-                pass
-        sys.exit(1)
+        print("warn, forbidden warning:", m.group(2), file=sys.stderr)
+        return
 
 def run_gcc():
     args = sys.argv[1:]
