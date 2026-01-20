163c07a3b065c9f44b4b66f35caaececc8e75cad
tanran <51186467+tanranv5@users.noreply.github.com>
2026-01-20 21:01:16 +0800

    module: ignore modversion mismatches for vendor modules

--- kernel/module.c
+++ kernel/module.c
@@ -1320,9 +1320,9 @@ static int check_version(const struct load_info *info,
 	return 1;
 
 bad_version:
-	pr_warn("%s: disagrees about version of symbol %s\n",
-	       info->name, symname);
-	return 0;
+	pr_warn("%s: ignore version mismatch for symbol %s\n",
+		info->name, symname);
+	return 1;
 }
 
 static inline int check_modstruct_version(const struct load_info *info,
